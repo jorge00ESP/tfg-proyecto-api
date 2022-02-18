dc56679c426766581d316dfe4a3e939c2dfca4fc
jorge00ESP <90790857+jorge00ESP@users.noreply.github.com>
2022-02-18 18:28:04 +0100

    tokens
    api preparada para login con tokens

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -54,6 +55,8 @@ class UserController extends Controller
                 'data'    => null
             ]);
         }
+
+        $data['password']=Hash::make($data['password']);
 
         try{
             DB::table('users')->insert($data);
@@ -163,7 +166,7 @@ class UserController extends Controller
         }
 
         if(!empty($password)){
-            $data['password']=$password;
+            $data['password']=Hash::make($data['password']);
         }
 
         if(!empty($email)){
